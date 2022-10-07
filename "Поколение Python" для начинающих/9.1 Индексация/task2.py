31b78618b4eef099f150467942c072f766addc11
Valeria <89817818+Alepsny@users.noreply.github.com>
2022-10-07 19:16:36 +0300

    Create task2.py

---
+++ "Поколение Python" для начинающих/9.1 Индексация/task2.py
@@ -0,0 +1,9 @@
+'''
+Дополните приведенный код, используя индексатор, так чтобы он вывел символ w.
+'''
+
+
+s = "In 2010, someone paid 10k Bitcoin for two pizzas."
+for i in range(len(s)):
+    if s[i] == "w":
+        print(s[i])
