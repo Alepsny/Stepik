6d9bb46b071440420d981c277869bcdc2bf88ab3
Valeria <89817818+Alepsny@users.noreply.github.com>
2022-10-31 18:28:56 +0300

    Create pangram.py

---
+++ "Поколение Python" для начинающих/14.1 Задачи на программирование/pangram.py
@@ -0,0 +1,37 @@
+'''
+Панграмма – это фраза, содержащая в себе все буквы алфавита. Обычно панграммы используют для презентации шрифтов, чтобы можно было в одной фразе рассмотреть все глифы.
+
+Напишите функцию, is_pangram(text) которая принимает в качестве аргумента строку текста на английском языке и возвращает значение True если текст является панграммой 
+и False в противном случае.
+
+Примечание 1. Гарантируется, что введенная строка содержит только буквы английского алфавита.
+
+Примечание 2. Следующий программный код:
+
+print(is_pangram('Jackdaws love my big sphinx of quartz'))
+print(is_pangram('The jay pig fox zebra and my wolves quack'))
+print(is_pangram('Hello world'))
+должен выводить:
+
+True
+True
+False
+'''
+
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+
+# объявление функции
+def is_pangram(text):
+    for i in range(len(abc)):
+        if abc[i] in text.lower():
+            continue
+        else:
+            return False
+    return True
+       
+# считываем данные
+text = input()
+
+# вызываем функцию
+print(is_pangram(text))
