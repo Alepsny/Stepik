ea086e255e2ec261fc3846e2e5d96686478abd1a
Valeria <89817818+Alepsny@users.noreply.github.com>
2022-10-31 18:01:57 +0300

    Create is_a_number_prime?.py

---
+++ "Поколение Python" для начинающих/13.5 Функции с возвратом значения. Часть 2/is_a_number_prime?.py
@@ -0,0 +1,30 @@
+'''
+Напишите функцию is_prime(num), которая принимает в качестве аргумента натуральное число и возвращает значение True если число является простым и False в противном случае.
+
+ Примечание. Следующий программный код:
+
+print(is_prime(1))
+print(is_prime(10))
+print(is_prime(17))
+должен выводить:
+
+False
+False
+True
+'''
+
+
+# объявление функции
+def is_prime(num):
+    if num == 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(is_prime(n))
