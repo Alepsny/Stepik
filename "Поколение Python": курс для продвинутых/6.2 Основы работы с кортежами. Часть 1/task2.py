deb49b9dab7fe56344850f5bdd1c215f6c4f5282
Valeria <89817818+Alepsny@users.noreply.github.com>
2022-12-02 00:22:14 +0300

    Create task2.py

---
+++ "Поколение Python": курс для продвинутых/6.2 Основы работы с кортежами. Часть 1/task2.py
@@ -0,0 +1,9 @@
+'''
+countries = ('Russia', 'Argentina', 'Spain', 'Slovakia', 'Canada', 'Slovenia', 'Italy')
+last = countries[-1]
+print(last)
+'''
+
+
+primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71) 
+print(primes[:6])
