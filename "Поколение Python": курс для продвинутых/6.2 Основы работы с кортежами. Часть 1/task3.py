4e35f4c46e616d1818784432d74124a101343aae
Valeria <89817818+Alepsny@users.noreply.github.com>
2022-12-02 00:22:39 +0300

    Create task3.py

---
+++ "Поколение Python": курс для продвинутых/6.2 Основы работы с кортежами. Часть 1/task3.py
@@ -0,0 +1,8 @@
+'''
+primes = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71) 
+print(primes[:6])
+'''
+
+
+countries = ('Russia', 'Argentina', 'Slovakia', 'Canada', 'Slovenia', 'Italy', 'Spain', 'Ukraine', 'Chile', 'Cameroon')
+print(countries[2:])
